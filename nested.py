29a6cd3c6ad1c4acebcb579396ba7fe7af09efea
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-01-06 20:52:47 +0200

    Add _path, _line_no variables

--- nested.py
+++ nested.py
@@ -7,12 +7,17 @@ import re
 
 
 if __name__ == '__main__':
+    _path: str = ''
+    _line_no: int = 1
     for path in pathlib.Path('.').rglob('*.py'):
         if path.exists():
             with path.open('rt', encoding='latin-1') as file:
+                _path = str(path)
+                _line_no = 1
                 for line in file:
                     m = re.match('.*(#.*)$', line)
                     if m:
                         comment = m.group(1)
                         if 'test_dir' in comment:
-                            print(comment)
+                            print(f'{_path = }, {_line_no = }, {comment = }')
+                    _line_no += 1
--- test_dir/get_paths.py
+++ test_dir/get_paths.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
-# file: get_paths test_dir
+
+# file: get_paths test_dir line: 5
 def get_paths(topdir, pattern):
     for path in pathlib.Path(topdir).rglob(pattern):
         if path.exists():
