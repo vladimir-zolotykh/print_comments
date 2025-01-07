aee7fa63687b2b27368f970ae886936a55fb4ab0
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-01-07 23:36:50 +0200

    Add&use Data namedtuple

--- comments.py
+++ comments.py
@@ -3,36 +3,42 @@
 # PYTHON_ARGCOMPLETE_OK
 import pathlib
 import re
+from collections import namedtuple
+Data = namedtuple('Data', 'path file line line_no match')
 
 
 def get_paths(topdir, pattern):
     for path in pathlib.Path(topdir).rglob(pattern):
         if path.exists():
-            yield path
+            yield Data(path, None, None, None, None)
 
 
 def get_files(paths):
     for path in paths:
-        with path.open('rt', encoding='latin-1') as file:
-            yield file
+        with path.path.open('rt', encoding='latin-1') as file:
+            yield Data(path.path, file, None, None, None)
 
 
 def get_lines(files):
     for file in files:
-        yield from file
+        line_no: int = 1
+        for line in file.file:
+            yield Data(file.path, file.file, line, line_no, None)
+            line_no += 1
+        # yield from file
 
 
 def get_comments(lines):
     for line in lines:
-        m = re.match('.*(#.*)$', line)
+        m = re.match('.*(#.*)$', line.line)
         if m:
-            yield m.group(1)
+            yield Data(line.path, line.file, line.line, line.line_no, m.group(1))
 
 
 def print_matching(lines, substring):
     for line in lines:
-        if substring in line:
-            print(line)
+        if substring in line.line:
+            print(f'{line.path = }, {line.line_no = }, {line.line = }')
 
 
 if __name__ == '__main__':
@@ -40,4 +46,4 @@ if __name__ == '__main__':
     files = get_files(paths)
     lines = get_lines(files)
     comments = get_comments(lines)
-    print_matching(comments, 'spam')
+    print_matching(comments, 'test_dir')
