da7d97f823c53bea471c256d6740b77cad93ac8e
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-01-09 20:55:55 +0200

    Use pydantic
    class Data(BaseMode): ...

--- comments.py
+++ comments.py
@@ -2,37 +2,45 @@
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
 import pathlib
+import io
 import re
 from collections import namedtuple
-from typing import Optional, NamedTuple
+from typing import Optional, Any
+from pydantic import BaseModel
 # Data = namedtuple('Data', 'path file line line_no match')
 
 
-class Data(NamedTuple):
-    path: Optional[str]
-    file: Optional[str]
+class Data(BaseModel):
+    path: pathlib.Path
+    file: Any
     line: Optional[str]
-    line_no: int
+    line_no: Optional[int]
     match: Optional[str]
+
+    @classmethod
+    def from_list(cls, *items):
+        options = {k: v for k, v in zip(cls.__annotations__, items)}
+        return cls(**options)
 
 
 def get_paths(topdir, pattern):
     for path in pathlib.Path(topdir).rglob(pattern):
         if path.exists():
-            yield Data(path, None, None, None, None)
+            yield Data.from_list(path, None, None, None, None)
 
 
 def get_files(paths):
     for path in paths:
         with path.path.open('rt', encoding='latin-1') as file:
-            yield Data(path.path, file, None, None, None)
+            print(f'{type(file) = }')
+            yield Data.from_list(path.path, file, None, None, None)
 
 
 def get_lines(files):
     for file in files:
         line_no: int = 1
         for line in file.file:
-            yield Data(file.path, file.file, line, line_no, None)
+            yield Data.from_list(file.path, file.file, line, line_no, None)
             line_no += 1
         # yield from file
 
@@ -41,14 +49,15 @@ def get_comments(lines):
     for line in lines:
         m = re.match('.*(#.*)$', line.line)
         if m:
-            yield Data(line.path, line.file, line.line, line.line_no, m.group(1))
+            yield Data.from_list(line.path, line.file, line.line,
+                                 line.line_no, m.group(1))
 
 
 def print_matching(lines, substring):
     for line in lines:
         if substring in line.line:
-            print("{:40s}:{:d} {:s}".format(str(line.path), line.line_no, line.line))
-            # print(f'{str(line.path) = }, {line.line_no = }, {line.line = }')
+            print("{:40s}:{:d} {:s}".format(str(line.path), line.line_no,
+                                            line.line))
 
 
 if __name__ == '__main__':
